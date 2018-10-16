6b6d49d11794ce62cc880433cb4ca278556e3b67
20150477 <ejrdnjs96@gmail.com>
2018-10-16 12:59:48 +0900

    Update Soft04_2.java

--- 퀴즈4/Soft04_2.java
+++ 퀴즈4/Soft04_2.java
@@ -1,4 +1,35 @@
 package soft4;
-
-
+class Soft_4 {
+  public int solution(int n) {
+      int answer = n;
+      int count=0;
+      int temp=n;
+      int count1=0;
+      while(n>1){
+          if(n%2==0)
+              n/=2;
+          else{
+              n/=2;
+              count++;
+          }
+      }
+      
+      while(true){
+          if(count==count1)
+              break;
+          count1=0;
+          answer++;
+          temp=answer;
+          while(temp>1){
+            if(temp%2==0)
+                temp/=2;
+            else{
+                temp/=2;
+                count1++;
+          }
+         }
+      }
+      
+      return answer;
+  }
 }
