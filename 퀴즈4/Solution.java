647a31f59c98f2e36a7ed0b210c371251a86c366
20150477 <ejrdnjs96@gmail.com>
2018-10-16 11:14:33 +0900

    Update Solution.java

--- 퀴즈4/Solution.java
+++ 퀴즈4/Solution.java
@@ -1,4 +1,28 @@
-
-public class Solution {
-
+class Solution {
+  public int solution(long num) {
+      int count=0;
+      while(num!=1){
+          if(count==500){
+              return -1;
+          }
+          if(num%2==0){
+              num/=2;
+              count++;
+          }
+          else{
+            num*=3;
+            num+=1;
+            count++;
+        }
+          if(num==1){
+              return count;
+          }
+          if(count==500){
+              return -1;
+          }
+      }
+      
+      int answer = count;
+      return answer;
+  }
 }
