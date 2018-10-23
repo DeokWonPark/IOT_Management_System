211e1d790aea890083bb70bbfc538f8f1f7bf77f
20150477 <ejrdnjs96@gmail.com>
2018-10-23 11:02:29 +0900

    Update Solution.java

--- 퀴즈5/Solution.java
+++ 퀴즈5/Solution.java
@@ -1,5 +1,24 @@
 package sol;
 
-public class Solution {
-
+class Solution {
+  public int solution(int n) {
+      int answer = 0;
+      int sum=0;
+      for(int i=0;i<n;i++){
+          sum=0;
+          for(int j=0;j<n;j++){
+              sum+=i+j+1;
+              if(sum==n){
+                  answer++;
+                  break;
+              }
+              else if(sum>n){
+                  break;
+              }
+                  
+          }
+      }
+          
+      return answer;
+  }
 }
