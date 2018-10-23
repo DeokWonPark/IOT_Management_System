6cd94429087b66437ac3b802d9eadd39cdb55902
20150477 <ejrdnjs96@gmail.com>
2018-10-23 11:34:29 +0900

    Update Solution2.java

--- 퀴즈5/Solution2.java
+++ 퀴즈5/Solution2.java
@@ -1,5 +1,15 @@
 package sol;
 
-public class Solution2 {
-
+class Solution2 {
+  public long solution(int n) {
+      long answer = 1;
+      long before=1;
+      long mid=1;
+      for(int i=2;i<=n;i++){
+          answer=((mid+before)%1234567);
+          mid=before;
+          before=answer;
+          }
+       return answer;
+  }
 }
